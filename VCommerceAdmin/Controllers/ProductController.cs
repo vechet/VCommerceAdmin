7e9a217a5ee6ac148b6a88d5aa72e10c9da6819c
vechet <vechet240@gmail.com>
2023-09-12 16:59:53 +0700

    adding admin template

---
+++ VCommerceAdmin/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VCommerceAdmin.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
